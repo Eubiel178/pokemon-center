2f7a1fd77507a3bd4d83db816a1e36faf5c92c82
Gabriel <dev123gabriel@gmail.com>
2024-01-27 18:29:26 -0300

    :ok_hand: style: height do pai definido

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -3,4 +3,12 @@ import { ResetStyle } from "./ResetStyle";
 
 export const GlobalStyle = createGlobalStyle`
   ${ResetStyle}
+
+  #__next{
+    height: 100dvh;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+  }
 `;
