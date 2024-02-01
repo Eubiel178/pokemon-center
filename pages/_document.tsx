fcf0b6f6ea4e8407f6cc7e1e16ff56890eece7a7
Gabriel <dev123gabriel@gmail.com>
2024-02-01 19:54:08 -0300

    :rocket: enviando para deploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <title>Centro Pokemon</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
