0f44399ed41c4f4f47613813a046976768bd5a1a
Gabriel <dev123gabriel@gmail.com>
2024-02-02 08:27:26 -0300

    :ok_hand: criação de modalWrapper

--- components/atoms/ModalToast/ModalText/styles.ts
+++ components/atoms/ModalToast/ModalText/styles.ts
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Text = styled.p`
   font-weight: 400;
-  font-size: 0.875rem;
+  font-size: 0.9375rem;
   line-height: 1.25rem;
 `;
---
+++ components/atoms/ModalToast/ModalWrapper/index.tsx
@@ -0,0 +1,5 @@
+import * as S from "./styles";
+
+export const ModalWrapper = ({ children }: React.ComponentProps<"div">) => {
+  return <S.Wrapper>{children}</S.Wrapper>;
+};
